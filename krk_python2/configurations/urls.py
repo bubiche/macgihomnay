7a9fb1e9113a77221815959a25aa4dcd5e25c4e5
Bao Truong <tmbao@localhost.localdomain>
2015-11-13 09:50:35 +0700

    First commit

---
+++ krk_python2/configurations/urls.py
@@ -0,0 +1,30 @@
+"""krk URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from oscar.app import application
+from django.conf import settings
+from django.conf.urls.static import static
+
+from paypal.express.dashboard.app import application as app_paypal
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'', include(application.urls)),
+    (r'^checkout/paypal/', include('paypal.express.urls')),
+	(r'^dashboard/paypal/express/', include(app_paypal.urls)),
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
